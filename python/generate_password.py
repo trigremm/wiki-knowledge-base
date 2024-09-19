50e9ae18af250f6c4748e49e6212fd22dc500609
Askhat Z6 Molkenov <askhat.molkenov@gmail.com>
2024-09-19 08:57:58 +0500

    file added

---
+++ python/generate_password.py
@@ -0,0 +1,3 @@
+# from uuid import uuid4; print("\n".join(f"{str(uuid4())}-{str(uuid4()).upper()}" for _ in range(20)))
+
+from uuid import uuid4; print("\n".join(f"{str(uuid4())}-{str(uuid4()).upper()}" for _ in range(20)))
