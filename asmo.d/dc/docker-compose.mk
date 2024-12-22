bf20cb10ad129d44b7331a361e3a0241110ffffb
Molkenov Askhat <askhat.molkenov@gmail.com>
2024-12-22 11:29:46 +0500

    reanme makefile

--- asmo.d/dc/docker-compose.mk
+++ asmo.d/dc/docker-compose.mk
@@ -1,47 +1,62 @@
-UTILS_DIR := utils
 
 DC_BIN := docker compose
 MAIN_SERVICE_NAME := python_project_template
 
-.PHONY: all gitinfo ps logs 
+.PHONY: all gitinfo ps logs
 .PHONY: docker-stop-all prune-containers prune-system prune prune-containers
-.PHONY: pull build up stop down restart recreate 
+.PHONY: pull build up stop down restart recreate
 .PHONY: install_uv install_dev shell runshell ishell
 .PHONY: format
 .PHONY: l s i r rl rs ri f
-
 
 export GIT_COMMIT_ID=$(shell git rev-parse HEAD)
 export GIT_COMMIT_DATE=$(shell git log -1 --format=%cd --date=iso)
 export GIT_COMMIT_MESSAGE=$(shell git log -1 --pretty=%B)
 
-all: ps
+# default command
+.PHONY: help
+help:
+	@echo 'This is a Makefile for docker-compose commands'
+	@echo 'It has docker, git, docker-compose commands'
 
+# docker commands
+.PHONY: docker-stop-all prune-containers prune-volumes prune-system prune
 docker-stop-all:
+	# docker stop $(docker ps -a -q)
 	@echo 'docker stop $$(docker ps -a -q)'
 	@echo docker ps -a
 
-gitinfo:
-	@echo "Git Commit ID: $(GIT_COMMIT_ID)"
-	@echo "Git Commit Date: $(GIT_COMMIT_DATE)"
-	@echo "Git Commit Message: $(GIT_COMMIT_MESSAGE)"
-
-ps: gitinfo
-	$(DC_BIN) ps
-
-logs:
-	while true; do $(DC_BIN) logs -f --tail=100 ; sleep 10; done
-
 prune-containers:
+	docker container ls -a
 	docker container prune -f
+	docker container rm $(docker container ls -q)
+
+prune-volumes:
+	docker volume ls
+	docker volume prune -f
+	docker volume rm $(docker volume ls -q)
 
 prune-system:
 	docker system prune -f -a
 
 prune: prune-containers
 
+# git commands
+.PHONY: gitinfo pull
+gitinfo:
+	@echo "Git Commit ID: $(GIT_COMMIT_ID)"
+	@echo "Git Commit Date: $(GIT_COMMIT_DATE)"
+	@echo "Git Commit Message: $(GIT_COMMIT_MESSAGE)"
+
 pull:
 	git pull || echo 'some error '
+
+# docker-compose commands
+ps: gitinfo
+	$(DC_BIN) ps
+
+logs:
+	while true; do $(DC_BIN) logs -f --tail=100 ; sleep 10; done
 
 build:
 	$(DC_BIN) build
@@ -96,8 +111,3 @@ rs: r s
 ri: r i
 
 f: format
-
-# fallback commands
-%:
-	@echo try to run command in other locations
-	$(MAKE) -C $(UTILS_DIR) $@ || echo 'no target found'
